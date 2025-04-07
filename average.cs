5a80cf6d4dd4e2e38a09d607d3b19220e229eb01
TheTohru <yutresuf09@gmail.com>
2025-04-07 07:59:14 +0300

    average.cs

---
+++ average.cs
@@ -0,0 +1,62 @@
+public static void average()
+{
+    Console.Clear();
+    Console.ForegroundColor = ConsoleColor.Blue;
+    Console.WriteLine("Average selected...");
+    Console.ForegroundColor = ConsoleColor.White;
+
+    Console.WriteLine("Please enter number one and two for transaction");
+
+    bool error = true;
+    while (error)
+    {
+        try
+        {
+            Console.Write("please enter number one: ");
+            int one = Convert.ToInt32(Console.ReadLine());
+            Console.Write("please enter number two: ");
+            int two = Convert.ToInt32(Console.ReadLine());
+
+            int value = (one + two) / 2;
+            Console.WriteLine(@"
+            ");
+            Console.WriteLine("procces completed");
+            Console.WriteLine("number one: " + one);
+            Console.WriteLine("number two: " + two);
+            Console.WriteLine("value: " + value);
+            Console.WriteLine(@"
+            ");
+            calculator.list1.Add($"Action history name: Average");
+            calculator.list1.Add($"number one: {one}");
+            calculator.list1.Add($"number two: {two}");
+            calculator.list1.Add($"value: {value}");
+
+            error = false;
+        }
+        catch (FormatException)
+        {
+            Console.ForegroundColor = ConsoleColor.Red;
+            Console.WriteLine("Invalid input, please enter a valid number.");
+            Console.ForegroundColor = ConsoleColor.White;
+        }
+
+        Console.WriteLine(@"
+    ");
+        Console.WriteLine(" Do you want another action? (y/n)");
+        Console.ForegroundColor = ConsoleColor.Blue;
+        Console.Write("=> ");
+        Console.ForegroundColor = ConsoleColor.White;
+        string choice = Console.ReadLine();
+        switch (choice)
+        {
+            case "y":
+                Console.Clear();
+                calculator.menu();
+                break;
+
+            case "n":
+                Console.Clear();
+                Console.WriteLine("Calculator shuts down...");
+                break;
+        }
+    }
